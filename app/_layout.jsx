b70fabe06f8cfd9dea5f7482e2f268de36b53a4b
Loges <sivalog25@gmail.com>
2024-04-28 11:59:22 +0530

    initial commit

---
+++ app/_layout.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Loges S 20MIS1100</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

3a26c1fb91bb1833adb2b79c6cbc6912285b9cc6
Loges <sivalog25@gmail.com>
2024-06-04 23:30:51 +0530

    search bar

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,15 +1,58 @@
-//dynamic routing
-//[query], page inside [] will help us to rename the page or 
-//get the query info for the search that user is trying to do 
-
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInputs from '../../components/SearchInputs';
+import EmptyState from '../../components/EmptyState';
+import { searchPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
+import { useLocalSearchParams } from 'expo-router';
 
 const Search = () => {
+
+  const { query } = useLocalSearchParams()
+
+  //we use useAppwrite to make the call to searchPosts 
+  const { data: posts, refetch } = useAppwrite(
+    ()=> searchPosts(query)
+  );
+
+  console.log(query,posts);
+
+  useEffect(() => {
+    refetch()
+  }, [query])
+
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full" >
+      <FlatList
+        data={posts}
+        //data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">Search results</Text>
+            <Text className="text-2xl font-psemibold text-white">{query}</Text>
+            <View className="mt-6 mb-8">
+             <SearchInputs initialQuery={query} />
+            </View>
+          </View>
+        )}
+        //what will happen if our list is empty
+        ListEmptyComponent={() => (
+          //<Text className="text-white">Empty</Text>
+          <EmptyState
+            title="NO VIDEOS FOUND"
+            subtitle="No Videos found for this search query"
+          />
+        )}
+
+      />
+    </SafeAreaView>
   )
 }
 
